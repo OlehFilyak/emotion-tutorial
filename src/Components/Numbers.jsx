ed51bebdbbb7d82e04c2e53eb04690e4c983bfb6
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-18 11:55:04 +0300

    Create Numbers.jsx

---
+++ src/Components/Numbers.jsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+
+function Numbers() {
+  return (
+    <div
+      css={{
+        padding: 8,
+        zIndex: 200,
+      }}
+    >
+      Тут додано padding 8px і z-index 200. Числа Когда числа являются значением
+      свойства css, к числу добавляется px, если только это не свойство css,
+      которое не является единицей измерения.
+    </div>
+  );
+}
+
+export default Numbers;
