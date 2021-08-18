3e7be2751ed11283449cb44b825db06cbb37a616
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-18 11:55:09 +0300

    Create Array.jsx

---
+++ src/Components/Array.jsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
+function Array() {
+  return (
+    <div
+      css={[
+        { color: "darkorchid" },
+        { backgroundColor: "hotpink" },
+        { padding: 8 },
+      ]}
+    >
+      Властивості цього тексту передані масивом об'єктів
+    </div>
+  );
+}
+
+export default Array;
